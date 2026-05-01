3f25f325cb6a26c090a16173af6e59272e3bf6c9
quantumpod <90433921+quantumpod@users.noreply.github.com>
2026-04-30 21:54:09 -0700

    Update vite.config.ts to configure Vite with React and TailwindCSS

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/productdetails/',
   plugins: [
     // The React and Tailwind plugins are both required for Make, even if
     // Tailwind is not being actively used – do not remove them
@@ -14,5 +15,8 @@ export default defineConfig({
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
+  },
+  build: {
+    outDir: 'dist',
   },
 })
